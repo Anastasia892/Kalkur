fc6d4c6a02cf01bb833b2a6b2586c04db8d57edf
Владелец <@DESKTOP-1NJN28O>
2022-01-26 20:58:46 +0300

    Добавьте файлы проекта.

---
+++ Kalkur/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kalkur
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+   
+        public MainWindow()
+        {
+            InitializeComponent();
+            model.tbItog = Tb6;
+            CBox1.ItemsSource = model.datalist;
+        }
+
+        private void vich_Click(object sender, RoutedEventArgs e)
+        {
+            model.oneSV = Convert.ToInt32(Tb2.Text);
+            model.twoSV = Convert.ToInt32(Tb4.Text);
+            model.idSV = CBox1.SelectedIndex;
+            Tb6.Text = model.Rezultat;
+        }
+
+        private void CBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            model.idSV = CBox1.SelectedIndex;
+            Tb3.Text = model.Combo;
+        }
+    }
+}
---
+++ Kalkur/model.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Kalkur
+{
+   static class model
+    {
+        static int one = 0;
+        static float two = 0;
+        static int id = 0;
+        static float itog;
+        public static List<string> datalist = new List<string> { "Сложение", "Умножение", "Вычитание", "Деление" };
+       
+        public static TextBox tbItog;
+
+        public static int oneSV
+        {
+            set
+            {
+                one = value;
+            }
+        }
+        public static float twoSV
+        {
+            set
+            {
+                two = value;
+            }
+        }
+        public static int  idSV
+        {
+            set
+            {
+                id = value;
+            }
+        }
+        public static string Rezultat
+        {
+            get
+            {
+                switch(id)
+                {
+                    case 0:
+                        itog = one + two;
+                        return tbItog.Text=(itog).ToString();
+                    case 1:
+                        itog = one * two;
+                        return tbItog.Text = (itog).ToString();
+                    case 2:
+                        itog = one - two;
+                        return tbItog.Text = (itog).ToString();
+                    case 3:
+                        itog = one / two;
+                        return tbItog.Text = (itog).ToString();
+                    default:
+                        
+                        return "Введите снова";
+
+
+                }
+
+
+            }
+        }
+
+        public static string Combo
+        {
+           
+
+            get
+            {
+                switch (id)
+                {
+                    case 0:
+
+                        return "+";
+                    case 1:
+
+                        return "*";
+                    case 2:
+
+                        return "-";
+                    case 3:
+
+                        return "/";
+                    default:
+
+                        return "Введите снова";
+
+                }
+
+            }
+        }
+    }
+}
